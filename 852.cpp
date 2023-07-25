22a760e301409c9a17ec9e82df8ac1614e51b56a
perryHV <shagunagl7@gmail.com>
2023-07-25 11:39:05 +0530

    Initial commit

---
+++ 852.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n=arr.size();
+        int i=0,j=n;
+        while(i<j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]>arr[mid-1]) {
+                i=mid;
+            }
+            else j=mid-1
+        }
+        return i;
+    }
+};
---
+++ code.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dp[10002][10002];
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size(),n=mat[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]) dp[i][j]=INT_MAX;
+                else dp[i][j]=0;
+            }
+            for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]) dp[i][j]= solve(i,j,m,n);
+            }
+
+        }
+        return dp;
+    }
+    int solve(int i,int j, int m,int n){
+        if(i<0||i>=m||j<0||j>=n) return INT_MAX;
+        if(dp[i][j]!=INT_MAX) return dp[i][j];
+        int dist=INT_MAX;
+        dist=min(dist,1+solve(i+1,j+1,m,n));
+        dist=min(dist,1+solve(i+1,j-1,m,n));
+        dist=min(dist,1+solve(i-1,j+1,m,n));
+        dist=min(dist,1+solve(i-1,j-1,m,n));
+        
+       return dp[i][j]=dist;
+    }
+};
